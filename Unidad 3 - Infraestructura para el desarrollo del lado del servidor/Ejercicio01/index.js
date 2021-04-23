0bea6beeb46a03c6fde4a34bc2635c131427a092
DanielAyalaRamirez <daalayalara@ittepic.edu.mx>
2021-04-23 15:49:47 -0600

    Orden de carpetas y ejercicios U3

---
+++ Unidad 3 - Infraestructura para el desarrollo del lado del servidor/Ejercicio01/index.js
@@ -0,0 +1,7 @@
+let squar = require("./square");
+//.. nos indica salir de la carpeta "../otraCarpeta/square"
+
+let a = squar.area(5);
+let p = squar.perimetro(5);
+
+console.log("El area es: " + a + " ,El perimetro es: "+p);
---
+++ Unidad 3 - Infraestructura para el desarrollo del lado del servidor/Ejercicio01/square.js
@@ -0,0 +1,10 @@
+function area(width){
+    return width * width;
+}
+
+function perimetro(width){
+    return width * 4
+}
+
+module.exports.area = area;
+module.exports.perimetro = perimetro;
