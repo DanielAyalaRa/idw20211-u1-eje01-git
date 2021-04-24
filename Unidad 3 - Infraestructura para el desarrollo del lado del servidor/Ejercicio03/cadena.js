7945793ef45518692230d75d7cb27ebe6671f356
DanielAyalaRamirez <daalayalara@ittepic.edu.mx>
2021-04-23 22:24:35 -0600

    v0.2.0

--- Unidad 3 - Infraestructura para el desarrollo del lado del servidor/Ejercicio03/cadena.js
+++ Unidad 3 - Infraestructura para el desarrollo del lado del servidor/Ejercicio03/cadena.js
@@ -1,18 +1,28 @@
 function palindromo(cadena){
+    //ponemos en minusculas la cadena
     var cad = cadena.toLowerCase();
-    var sinEspacio = cad.replace(" ","");
-
-    var n = sinEspacio.length;
-    var cad_1 = sinEspacio;
-    var cad_2 = sinEspacio; 
+    //Separar palabras por medio del split
+    var separar = cad.split(" ");
+    //Contar numero de palabras
+    var nPalabras = separar.length;
+    //Eliminamos los espacios en blanco con el replace
+    var nuevaCad = cad.replace(/ /g,"");
+    //Numero de vocales
+    var vocales = nuevaCad.match(/[aeiou]/gi).length;
+    //Numero de vocales
+    var consonantes = nuevaCad.match(/[bcdfghjklmnñpqrstvwxyz]/gi).length;
+    //Obtenemos el numero de letras de la cadena sin espacio
+    var n = nuevaCad.length;
+    //validacion de palindromo 
     var igual = true;
-
+    //inicio de la cadena
     var i = 0;
+    //final al inicio de la cadena
     var j = n-1;
-
+    
     while (i <= n){
-        if (cad_1.charAt(i) == cad_2.charAt(j)){
-            //Todo bien
+        if (nuevaCad.charAt(i) == nuevaCad.charAt(j)){
+            //todo bien
         }
         else{
             igual = false;
@@ -21,13 +31,29 @@ function palindromo(cadena){
         if(j<=0){
             break;
         }
+        /*Incrementamos y decrementamos variables
+          para mover de posicion la cadena*/
         i++;
         j--;
     }//Validacion
+
+    //Imprimimos resultados
     if (igual == true){
-        console.log("Si es palindromo");
+        console.log("Cadena: " + cadena + "\n"
+                    +"Si es palindromo \n"
+                    +"Número de palabras: " + nPalabras + "\n"
+                    +"Número de letras: " + n + "\n"
+                    +"Número de vocales: " + vocales + "\n"
+                    +"Número de consonantes: " + consonantes + "\n"
+                    );
     }else{
-        console.log("No es palindromo");
+        console.log("Cadena: " + cadena + "\n"
+                    +"No es palindromo \n"
+                    +"Número de palabras: " + nPalabras + "\n"
+                    +"Número de letras: " + n + "\n"
+                    +"Número de vocales: " + vocales + "\n"
+                    +"Número de consonantes: " + consonantes + "\n"
+                    );
     }
 }
 
--- Unidad 3 - Infraestructura para el desarrollo del lado del servidor/Ejercicio03/index.js
+++ Unidad 3 - Infraestructura para el desarrollo del lado del servidor/Ejercicio03/index.js
@@ -1,6 +1,24 @@
 let cad = require("./cadena");
 
-let p = cad.principal;
+let p = cad.principal("Somos o no somos");
 
 //Imprimir en consola
-console.log(p("Ana"));
+console.log(p);
+
+/*
+- EJEMPLO DE PALINDROMOS -
+
+ANA
+
+Anita lava la TINA
+
+A la Manuela dale una mala
+
+A mama Roma le aviva el amor a papa y a papa Roma le aviva el amor a mama
+
+Anita la gorda lagartona no traga la droga latina
+
+Oso loco no coloso
+
+Somos o no somos
+*/
